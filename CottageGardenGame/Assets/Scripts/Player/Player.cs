f16ad440b200988f26ab57ed0bd049ab696f6722
Eva <evavanderscheer@live.nl>
2021-11-23 18:01:29 +0100

    Player movement

---
+++ CottageGardenGame/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private string Name;
+    //inventory
+    //wallet
+
+    private Rigidbody2D body;
+
+    private float moveX;
+    private float moveY;
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(moveX * speed, moveY * speed);
+    }
+
+
+}
